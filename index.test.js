a0c631d37fbab463251389f13c2ee8237d7a1222
jolle <jolle@users.noreply.github.com>
2017-09-23 23:41:20 +0300

    🚨 Increase test timeout

--- index.test.js
+++ index.test.js
@@ -2,6 +2,8 @@ const desktopper = require('./index');
 const path = require('path');
 const del = require('del')
 
+jest.setTimeout(3 * 60 * 1000);
+
 test(
     'it returns a correct path',
     done => desktopper('https://quizlet.com') // maybe some other website to test with?
