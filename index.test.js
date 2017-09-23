ccb7dccd50a5c8fba1f5fdd636d3dcfa1401cfb5
jolle <jolle@users.noreply.github.com>
2017-09-23 23:38:21 +0300

    🚨 Fiddle around with tests

--- index.test.js
+++ index.test.js
@@ -4,7 +4,8 @@ const del = require('del')
 
 test(
     'it returns a correct path',
-    () => desktopper('https://quizlet.com') // maybe some other website to test with?
+    done => desktopper('https://quizlet.com') // maybe some other website to test with?
         .then(res => expect(res).toBe(path.join(__dirname, 'Quizlet.app')))
         .then(() => del('Quizlet.app'))
+        .then(() => done())
 );
